13f44252e7200a584825c0a5b44f337b01bcb2e2
github username <github email address>
2018-06-10 23:46:38 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ActiveRecord::Base
+
+
+    VALID_CATEGORIES = ["Fiction", "Non-Fiction"]
+
+    validates :title, presence: true
+    validates :category, inclusion: { in: VALID_CATEGORIES }
+    validates :content, length: { minimum: 100 }
+
+
 end
